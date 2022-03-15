bc1abcfa1666f3f3d18a5949597a7eb5ce50d319
luffy0120 <jackyteng0120@gmail.com>
2022-03-15 14:01:28 +0800

    Initial commit

---
+++ src/main/java/com/tom/Hello.java
@@ -0,0 +1,9 @@
+package com.tom;
+
+public class Hello {
+    public static void main(String[] args) {
+//         new Person().hello();
+        Person p = new Person();
+        p.hello();
+    }
+}
---
+++ src/main/kotlin/com/tom/Hello Kotlin.kt
@@ -0,0 +1,12 @@
+package com.tom
+
+fun main(args: Array<String>) {
+//    println("Hello Kotlin")
+      Human().hello()
+}
+
+class Human {
+    fun hello(){
+        println("Hello kotlin")
+    }
+}
